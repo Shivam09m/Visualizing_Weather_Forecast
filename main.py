8b64c7887d0c89cbff6c9d10a6870f68be4620a2
Shivam Mehrotra <shivammehrotra09@yahoo.in>
2022-12-30 18:22:40 +0530

    Added Try Except case

--- main.py
+++ main.py
@@ -12,21 +12,25 @@ option = st.selectbox("Select data to view",
 st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
-    # Get the temperature/Sky data
-    filtered_data = get_data(place, days)
+    try:
+        # Get the temperature/Sky data
+        filtered_data = get_data(place, days)
 
-    if option == "Temperature":
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        temperatures = [x-273 for x in temperatures]
-        dates = [dict["dt_txt"] for dict in filtered_data]
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] for dict in filtered_data]
+            temperatures = [x-273 for x in temperatures]
+            dates = [dict["dt_txt"] for dict in filtered_data]
 
-        # Create a temperature plot
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature(C)"})
-        st.plotly_chart(figure)
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature(C)"})
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-        image_path = [images[condition] for condition in sky_conditions]
-        st.image(image_path, width=115)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_path = [images[condition] for condition in sky_conditions]
+            st.image(image_path, width=115)
+
+    except KeyError:
+        st.write("Place Does not Exists in Data")
